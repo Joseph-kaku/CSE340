4a6569e445720849bfc55423efd2e10e47af9d0f
Joseph-kaku <samkaks18@gmail.com>
2023-05-10 09:48:49 -0600

    next query added

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -2,4 +2,6 @@ INSERT INTO public.account(
 	account_firstname, account_lastname, account_email, account_password)
 	VALUES ( 'Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
-    
+UPDATE public.account
+	SET account_type= 'Admin'
+	WHERE account_id = 1;
